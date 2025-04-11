edb0dd2907f1dba03e7e6f4db3f8768ddb31cfcf
Michal Majewski <majesxddd@gmail.com>
2025-04-11 22:54:29 +0200

    rozszerzanie w toku

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,9 @@
 function summary_active() {
-    document.getElementById('summary').classList.toggle('summary-active')
+    document.getElementById('summary').classList.toggle('summary-active');
+}
+
+function reservation_clicked(clicked) {
+    document.getElementById('summary').classList.remove('summary-active');
+    clicked.classList.add('reservation-active')
+    clicked.classList.remove('reservation')
 }
