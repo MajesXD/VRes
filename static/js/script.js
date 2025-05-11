4caaafa0b5a3f24724e11258cb065824855fdcaa
MajesXD <majesxddd@gmail.com>
2025-05-11 14:46:54 +0200

    skonczony wyglad okna

--- static/js/script.js
+++ static/js/script.js
@@ -442,7 +442,7 @@ function reservationFull(event) {
 
     // Pełna rezerwacja (kliknięta)
      container_full.innerHTML = `
-        <div class="reservation_window_inside" id="reservation_window_inside">
+        <form class="reservation_window_inside" id="reservation_window_inside">
             <div class="reservation_window_top">
                 <svg style="color: ${active_color}" viewBox="0 0 22 25" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 22.6562C0 23.9502 1.0498 25 2.34375 25H19.5312C20.8252 25 21.875 23.9502 21.875 22.6562V9.375H0V22.6562ZM15.625 13.0859C15.625 12.7637 15.8887 12.5 16.2109 12.5H18.1641C18.4863 12.5 18.75 12.7637 18.75 13.0859V15.0391C18.75 15.3613 18.4863 15.625 18.1641 15.625H16.2109C15.8887 15.625 15.625 15.3613 15.625 15.0391V13.0859ZM15.625 19.3359C15.625 19.0137 15.8887 18.75 16.2109 18.75H18.1641C18.4863 18.75 18.75 19.0137 18.75 19.3359V21.2891C18.75 21.6113 18.4863 21.875 18.1641 21.875H16.2109C15.8887 21.875 15.625 21.6113 15.625 21.2891V19.3359ZM9.375 13.0859C9.375 12.7637 9.63867 12.5 9.96094 12.5H11.9141C12.2363 12.5 12.5 12.7637 12.5 13.0859V15.0391C12.5 15.3613 12.2363 15.625 11.9141 15.625H9.96094C9.63867 15.625 9.375 15.3613 9.375 15.0391V13.0859ZM9.375 19.3359C9.375 19.0137 9.63867 18.75 9.96094 18.75H11.9141C12.2363 18.75 12.5 19.0137 12.5 19.3359V21.2891C12.5 21.6113 12.2363 21.875 11.9141 21.875H9.96094C9.63867 21.875 9.375 21.6113 9.375 21.2891V19.3359ZM3.125 13.0859C3.125 12.7637 3.38867 12.5 3.71094 12.5H5.66406C5.98633 12.5 6.25 12.7637 6.25 13.0859V15.0391C6.25 15.3613 5.98633 15.625 5.66406 15.625H3.71094C3.38867 15.625 3.125 15.3613 3.125 15.0391V13.0859ZM3.125 19.3359C3.125 19.0137 3.38867 18.75 3.71094 18.75H5.66406C5.98633 18.75 6.25 19.0137 6.25 19.3359V21.2891C6.25 21.6113 5.98633 21.875 5.66406 21.875H3.71094C3.38867 21.875 3.125 21.6113 3.125 21.2891V19.3359ZM19.5312 3.125H17.1875V0.78125C17.1875 0.351562 16.8359 0 16.4062 0H14.8438C14.4141 0 14.0625 0.351562 14.0625 0.78125V3.125H7.8125V0.78125C7.8125 0.351562 7.46094 0 7.03125 0H5.46875C5.03906 0 4.6875 0.351562 4.6875 0.78125V3.125H2.34375C1.0498 3.125 0 4.1748 0 5.46875V7.8125H21.875V5.46875C21.875 4.1748 20.8252 3.125 19.5312 3.125Z"/>
@@ -494,6 +494,17 @@ function reservationFull(event) {
                             <p>${active_reservation.dataset.kwota}</p>
                             <p>${symbol_platnosci}</p>
                         </div>
+                        <select style="border: solid 2px ${active_color};">
+                            <option value="0"></option>
+                            <option value="1">Przelew</option>
+                            <option value="2">Karta</option>
+                            <option value="3">Gotówka</option>
+                            <option value="4">Voucher VR</option>
+                            <option value="5">Voucher MyBenefit</option>
+                            <option value="6">Voucher Katalog Marzeń</option>
+                            <option value="7">Voucher Katalog Prezentów</option>
+                            <option value="8">Voucher Super Prezenty</option>
+                        </select>
                     </div>
 
                     <div class="reservation_window_middle_left-row">
@@ -508,12 +519,6 @@ function reservationFull(event) {
                             </form>
                         </div>
                     </div>
-                        <div class="button_div">
-                            <div onclick="saveNote(${active_reservation.dataset.id})" class="reservation_window_middle_left-row add_note_button clickable" style="background-color: ${active_color};">
-                                Zapisz notkę
-                            </div>
-                        </div>
-
                 </div>
 
                 <div class="reservation_window_middle_right">
@@ -583,6 +588,7 @@ function reservationFull(event) {
 
                     <div class="reservation_window_middle_right-payment_row">
                         <select style="border: solid 2px ${active_color}">
+                            <option value="0"></option>
                             <option value="2">Karta</option>
                             <option value="3">Gotówka</option>
                         </select>
@@ -598,9 +604,11 @@ function reservationFull(event) {
                 
 
             <div class="reservation_window_bottom">
-                
+                <div onclick="saveNote(${active_reservation.dataset.id})" class="reservation_window_middle_left-row add_note_button clickable" style="background-color: ${active_color};">
+                Zapisz rezerwację
+                </div>
             </div>    
-        </div>
+        </form>
 `;
 
     const textareastyle = container_full.querySelector('textarea');
